f11f4864dcf88f7aa736a1fce9ff6c2abe839767
natan-morar <nsm120@bham.ac.uk>
2019-09-24 09:42:51 +0300

    first

--- src/DeltaConverter.js
+++ src/DeltaConverter.js
@@ -1,159 +1,231 @@
-var equal = require('deep-equal')
+var equal = require("deep-equal");
+const cheerio = require("cheerio");
 
 var escapeHtml = function(unsafe) {
-    if (unsafe) {
-
-    }
-    return unsafe
-		.replace(/</g, "&lt;")
-		.replace(/>/g, "&gt;")
-}
+  if (unsafe) {
+  }
+  return unsafe.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+};
 
 var wrapTag = function(tag, value, attrs) {
-    return "<" + tag + attrsToString(attrs) + ">" + value + "</" + tag + ">"
-}
+  return "<" + tag + attrsToString(attrs) + ">" + value + "</" + tag + ">";
+};
 
 var singleTag = function(tag, attrs) {
-    return "<" + tag + attrsToString(attrs) + ">"
-}
+  return "<" + tag + attrsToString(attrs) + ">";
+};
 
 var attrsToString = function(attrs) {
-    var attr_string = ""
-    if (attrs) {
-        for (var item in attrs) {
-            attr_string += " " + item + "='" + attrs[item] + "'"
-        }
+  var attr_string = "";
+  if (attrs) {
+    for (var item in attrs) {
+      attr_string += " " + item + "='" + attrs[item] + "'";
+      //if (item == "style") console.log(attrs);
     }
-    return attr_string
-}
+  }
+  return attr_string;
+};
 
 var convert = function(value, attributes) {
-    if (attributes) {
-        if (attributes['code-block']) {
-            return wrapTag("pre", wrapTag("code", value.join("\n").trim()))
-        } else if (attributes['header']) {
-            return wrapTag("h" + attributes['header'], value)
-        } else if (attributes['blockquote']) {
-            return wrapTag("blockquote", value)
-        } else if (attributes['list']) {
-            return wrapTag(attributes['list'] == 'ordered'? 'ol':'ul', value.map(function(item) { return wrapTag('li', item )}).join(""))
-        }
+  let qclass = "";
+  if (attributes) {
+    if (attributes["align"]) {
+      if (attributes["align"] == "center") qclass = "ql-align-center";
+      if (attributes["align"] == "left") qclass = "ql-align-left";
+      if (attributes["align"] == "right") qclass = "ql-align-right";
+      //console.log(qclass);
     }
+    if (attributes["code-block"]) {
+      return wrapTag("pre", wrapTag("code", value.join("\n").trim()), {
+        class: qclass
+      });
+    } else if (attributes["header"]) {
+      return wrapTag("h" + attributes["header"], value, {
+        class: qclass
+      });
+    } else if (attributes["blockquote"]) {
+      return wrapTag("blockquote", value, { class: qclass });
+    } else if (attributes["list"]) {
+      return wrapTag(
+        attributes["list"] == "ordered" ? "ol" : "ul",
+        value
+          .map(function(item) {
+            return wrapTag("li", item);
+          })
+          .join(""),
+        { class: qclass }
+      );
+    } else {
+      //console.log(value, attributes);
+    }
+  }
+  // ignores p class if iframe
+  //   if (!value.includes("iframe")) return wrapTag("p", value, { class: qclass });
+  //   else return wrapTag("p", value);
 
-    return wrapTag("p", value)
-}
+  return wrapTag("p", value, { class: qclass });
+};
 
 var convertInline = function(insert, attributes) {
-    if (attributes) {
-        if (attributes['bold']) {
-            return wrapTag("b", insert)
-        } else if (attributes['link']) {
-            return wrapTag("a", insert, {
-                "href": attributes.link
-            })
-        } else if (attributes['code']) {
-            return wrapTag("code", insert)
-        } else if (attributes['italic']) {
-            return wrapTag('i', insert)
-        }
+  let qstyle = "";
+  if (attributes) {
+    // if (attributes["color"]) {
+    //   qstyle += `color:${attributes["color"]};`;
+    // }
+    // if (attributes["background"]) {
+    //   qstyle += `background:${attributes["background"]};`;
+    // }
+    if (attributes["bold"]) {
+      return wrapTag("b", insert /*, { style: qstyle }*/);
+    } else if (attributes["link"]) {
+      return wrapTag("a", insert, {
+        href: attributes.link
+      });
+    } else if (attributes["code"]) {
+      return wrapTag("code", insert);
+    } else if (attributes["italic"]) {
+      return wrapTag("i", insert);
     }
+  }
 
-    return insert
-}
+  return insert;
+};
 
 var DeltaConverter = function(delta) {
-    this.results = ""
-    this.current = {
-        value: "",
-        attributes: null
-    }
-    this.delta = delta
-}
+  this.results = "";
+  this.current = {
+    value: "",
+    attributes: null
+  };
+  this.delta = delta;
+};
 
 DeltaConverter.prototype.addItem = function(insert, attributes) {
-    if (typeof insert === 'string' || insert instanceof String) {
-        if (insert.indexOf('\n') >= 0) {
-            if (equal(this.current.attributes, attributes) && attributes && (attributes['code-block'] || attributes['list']) && insert == "\n") {
-
-            } else {
-                this.convertPrevious()
-                this.current.attributes = attributes
-            }
-        }
-
-        this.current.value += convertInline(escapeHtml(insert), attributes)
-
-    } else if (insert.image) {
-        this.current.value += singleTag("img", {
-            "src": insert.image
-        })
-    } else if (insert.video) {
-        this.current.value += wrapTag("iframe", '', {
-            "src": insert.video,
-            "width": attributes.width,
-            "height": attributes.height
-        })
+  if (typeof insert === "string" || insert instanceof String) {
+    if (insert.indexOf("\n") >= 0) {
+      if (
+        equal(this.current.attributes, attributes) &&
+        attributes &&
+        (attributes["code-block"] || attributes["list"]) &&
+        insert == "\n"
+      ) {
+      } else {
+        this.convertPrevious();
+        this.current.attributes = attributes;
+      }
     }
-}
+    // if (attributes) {
+    //   console.log(`attributes  ${JSON.stringify(attributes)}`);
+    // }
+
+    this.current.value += convertInline(escapeHtml(insert), attributes);
+  } else if (typeof insert == "object") {
+    if (insert.image) {
+      this.current.value += singleTag("img", {
+        src: insert.image,
+        width: attributes && attributes.width ? attributes.width : "auto",
+        height: attributes && attributes.height ? attributes.height : "auto"
+      });
+    } else if (insert.video) {
+      //   this.current.value += convertInline(insert, attributes);
+      this.current.value += wrapTag("iframe", "", {
+        src: insert.video,
+        width: attributes && attributes.width ? attributes.width : "auto",
+        height: attributes && attributes.height ? attributes.height : "auto",
+        class: "ql-align-center"
+      });
+    }
+  }
+};
 
 DeltaConverter.prototype.convertPrevious = function() {
-    if (this.current.value.indexOf('\n') >= 0) {
-        var values = this.current.value.split('\n'),
-            length = values.length;
+  if (this.current.value.indexOf("\n") >= 0) {
+    var values = this.current.value.split("\n"),
+      length = values.length;
 
-        if (this.current.attributes && (this.current.attributes['code-block'] || this.current.attributes['list'])) {
-            var blockValues = []
-            for (var i = 0; i < length - 1; i++) {
-                blockValues.push(values[i])
-            }
-            this.results += convert(blockValues, this.current.attributes)
-        } else {
-            for (var i = 0; i < length - 1; i++) {
-                this.results += convert(values[i], this.current.attributes)
-            }
-        }
-        this.current.value = values[length - 1]
+    if (
+      this.current.attributes &&
+      (this.current.attributes["code-block"] || this.current.attributes["list"])
+    ) {
+      var blockValues = [];
+      for (var i = 0; i < length - 1; i++) {
+        blockValues.push(values[i]);
+      }
+      this.results += convert(blockValues, this.current.attributes);
+    } else {
+      for (var i = 0; i < length - 1; i++) {
+        this.results += convert(values[i], this.current.attributes);
+      }
     }
-}
+    this.current.value = values[length - 1];
+  }
+};
 
 DeltaConverter.prototype.convertRemain = function() {
-    if (this.current.value.indexOf('\n') >= 0) {
-        var values = this.current.value.split('\n'),
-            length = values.length,
-            results = ""
-        if (this.current.attributes && (this.current.attributes['code-block'] || this.current.attributes['list'])) {
-            var blockValues = []
-            for (var i = 0; i < length; i++) {
-                blockValues.push(values[i])
-            }
-            results += convert(blockValues, this.current.attributes)
-        } else {
-            for (var i = 0; i < length; i++) {
-                if (i == length - 1 && !values[i]) {
-                    break
-                }
-                results += convert(values[i], this.current.attributes)
-            }
-        }
-        return results
+  if (this.current.value.indexOf("\n") >= 0) {
+    var values = this.current.value.split("\n"),
+      length = values.length,
+      results = "";
+    if (
+      this.current.attributes &&
+      (this.current.attributes["code-block"] || this.current.attributes["list"])
+    ) {
+      var blockValues = [];
+      for (var i = 0; i < length; i++) {
+        blockValues.push(values[i]);
+      }
+      results += convert(blockValues, this.current.attributes);
     } else {
-        return convert(this.current.value, this.current.attributes)
+      for (var i = 0; i < length; i++) {
+        if (i == length - 1 && !values[i]) {
+          break;
+        }
+        results += convert(values[i], this.current.attributes);
+      }
     }
-}
+    return results;
+  } else {
+    return convert(this.current.value, this.current.attributes);
+  }
+};
 
 DeltaConverter.prototype.toHtml = function() {
-    var ops = this.delta.ops,
-        length = ops.length
+  var ops = this.delta.ops,
+    length = ops.length;
 
-    for (var i = 0; i < length; i++) {
-        var insert = ops[i].insert,
-            attributes = ops[i].attributes
+  for (var i = 0; i < length; i++) {
+    var insert = ops[i].insert,
+      attributes = ops[i].attributes;
 
-        this.addItem(insert, attributes)
-    }
+    this.addItem(insert, attributes);
+  }
 
-    this.results += this.convertRemain()
-    return this.results
-}
+  this.results += this.convertRemain();
 
-module.exports = DeltaConverter
+  //// replace functions
+  function escapeRegExp(str) {
+    return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+  }
+  function replaceAll(str, find, replace) {
+    return str.replace(new RegExp(escapeRegExp(find), "g"), replace);
+  }
+  // replace empty paragraphs with '&nbsp;'
+  this.results = replaceAll(this.results, "></p>", ">&nbsp;</p>");
+  ////
+  /// VIDEO ALIGN FIX
+  const $ = cheerio.load(this.results);
+  //   $("iframe[class='ql-align-center']")
+  //     .parent()
+  //     .addClass("ql-align-center");
+  //   $("iframe[class='ql-align-left']")
+  //     .parent()
+  //     .addClass("ql-align-left");
+  //   $("iframe[class='ql-align-right']")
+  //     .parent()
+  //     .addClass("ql-align-right");
+  ///
+
+  return $.html(); //this.results;
+};
+
+module.exports = DeltaConverter;
